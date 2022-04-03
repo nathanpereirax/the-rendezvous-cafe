a89c13b3b7509c9856ba75da0fb30ca59ed80f94
Nathan <nathan13pereira2001@gmail.com>
2022-04-03 07:57:43 +0000

    Added nodejs files

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static('../../the-rendezvous-cafe'))
 app.listen(3000)
