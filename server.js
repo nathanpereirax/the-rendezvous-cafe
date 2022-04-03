7e2ba211964918b11ad812a537af9782a0e94f6b
Nathan <nathan13pereira2001@gmail.com>
2022-04-03 14:20:35 +0000

    Changes to payment system

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+if(process.env.NODE_ENV !== 'production')
+{
+    require('dotenv').config()
+}
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+
+console.log(stripeSecretKey)
+
 const express = require('express')
 const app = express()
 
