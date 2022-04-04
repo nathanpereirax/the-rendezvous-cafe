dfd0ec2d2781531ed0f83c5c1908f9590c20b2c4
Nathan <nathan13pereira2001@gmail.com>
2022-04-04 17:17:06 +0000

    Shifted item info to json and itegrated it into the shop

--- server.js
+++ server.js
@@ -4,12 +4,34 @@ if(process.env.NODE_ENV !== 'production')
 }
 
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+const fs = require('fs')
 
 console.log(stripeSecretKey)
+console.log(stripePublicKey)
 
 const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+app.get('/menu', function(req, res)
+{
+    fs.readFile('items.json', function(error, data)
+    {
+        if(error)
+        {
+            res.status(500).end()
+        }
+        else
+        {
+            res.render('menu.ejs',
+            {
+                items: JSON.parse(data)
+            })
+        }
+    })
+})
+
 app.listen(3000)
