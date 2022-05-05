d7413d4ba4dd81aaf26922820f825feadd12ebba
Nathan <nathan13pereira2001@gmail.com>
2022-05-05 16:48:13 +0000

    Added MySQL support and accepting user info

--- public/js/store.js
+++ public/js/store.js
@@ -1,173 +1,199 @@
 if(document.readyState == 'loading')
 {
-    document.addEventListener('DOMContentLoaded', ready)
+		document.addEventListener('DOMContentLoaded', ready)
 }
 else
 {
-    ready()
+		ready()
 }
 
 function ready()
 {
-    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
-    for(var i=0; i < removeCartItemButtons.length; i++)
-    {
-        var button = removeCartItemButtons[i]
-        button.addEventListener('click', removeCartItem)
-    }
+		var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+		for(var i=0; i < removeCartItemButtons.length; i++)
+		{
+				var button = removeCartItemButtons[i]
+				button.addEventListener('click', removeCartItem)
+		}
 
-    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
-    for(var i=0; i < quantityInputs.length; i++)
-    {
-        var input = quantityInputs[i]
-        input.addEventListener('change', quantityChanged)
-    }
+		var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+		for(var i=0; i < quantityInputs.length; i++)
+		{
+				var input = quantityInputs[i]
+				input.addEventListener('change', quantityChanged)
+		}
 
-    var addToCartButtons = document.getElementsByClassName('shop-item-button')
-    for (var i = 0; i < addToCartButtons.length; i++)
-        {
-        var button = addToCartButtons[i]
-        button.addEventListener('click', addToCartClicked)
-    }
+		var addToCartButtons = document.getElementsByClassName('shop-item-button')
+		for (var i = 0; i < addToCartButtons.length; i++)
+				{
+				var button = addToCartButtons[i]
+				button.addEventListener('click', addToCartClicked)
+		}
 
-    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+		document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
 }
 
 function removeCartItem(event)
 {
-    var buttonClicked = event.target
-    buttonClicked.parentElement.parentElement.parentElement.remove()
-    updateCartTotal()
+		var buttonClicked = event.target
+		buttonClicked.parentElement.parentElement.parentElement.remove()
+		updateCartTotal()
 }
 
 function quantityChanged(event)
 {
-    var input = event.target
-    if(isNaN(input.value) || input.value <= 0)
-    {
-        input.value = 1
-    }
-    updateCartTotal()
+		var input = event.target
+		if(isNaN(input.value) || input.value <= 0)
+		{
+				input.value = 1
+		}
+		updateCartTotal()
 }
 
 function addToCartClicked(event)
 {
-    var button = event.target
-    var shopItem = button.parentElement.parentElement
-    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
-    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
-    var imageSrc = shopItem.getElementsByClassName('shop-item-img')[0].src
-    var id = shopItem.dataset.itemId
-    addItemToCart(title, price, imageSrc, id)
-    updateCartTotal()
+		var button = event.target
+		var shopItem = button.parentElement.parentElement
+		var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+		var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+		var imageSrc = shopItem.getElementsByClassName('shop-item-img')[0].src
+		var id = shopItem.dataset.itemId
+		addItemToCart(title, price, imageSrc, id)
+		updateCartTotal()
 }
 
 function addItemToCart(title, price, imageSrc, id)
 {
-    var cartRow = document.createElement('div')
-    cartRow.classList.add('cart-row')
-    cartRow.dataset.itemId = id
-    var cartImgBar = document.getElementsByClassName('cart-img-bar')[0]
-    var cartItemTitle = cartImgBar.getElementsByClassName('cart-item-title')
-    for (var i = 0; i < cartItemTitle.length; i++)
-    {
-        if (cartItemTitle[i].innerText == title)
-        {
-            alert('This item is already added to the cart. Increase the quantity in the cart to purchase more.')
-            return
-        }
-    }
-    var cartRowContents = `
-        <div class="cart-item">
-            <img class="shop-item-img" src="${imageSrc}">
-            <div class="item-info">
-                    <p class="shop-item-title cart-item-title">${title}</p>
-                </div>
-            </div>
-            <div class="cart-quantity">
-                <div class="price">
-                    <span>Price: </span>${price}
-                </div>
-                <div class="quantity">
-                    Quantity:
-                    <input class="cart-quantity-input" type="number" value="1">
-                </div>
-                <div class="remove">
-                    <button class="btn btn-danger" type="button">REMOVE</button>
-                </div>
-            </div>`
-    cartRow.innerHTML = cartRowContents
-    cartImgBar.append(cartRow)
-    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
-    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+		var cartRow = document.createElement('div')
+		cartRow.classList.add('cart-row')
+		cartRow.dataset.itemId = id
+		var cartImgBar = document.getElementsByClassName('cart-img-bar')[0]
+		var cartItemTitle = cartImgBar.getElementsByClassName('cart-item-title')
+		for (var i = 0; i < cartItemTitle.length; i++)
+		{
+				if (cartItemTitle[i].innerText == title)
+				{
+						alert('This item is already added to the cart. Increase the quantity in the cart to purchase more.')
+						return
+				}
+		}
+		var cartRowContents = `
+				<div class="cart-item">
+						<img class="shop-item-img" src="${imageSrc}">
+						<div class="item-info">
+										<p class="shop-item-title cart-item-title">${title}</p>
+								</div>
+						</div>
+						<div class="cart-quantity">
+								<div class="price">
+										<span>Price: </span>${price}
+								</div>
+								<div class="quantity">
+										Quantity:
+										<input class="cart-quantity-input" type="number" value="1">
+								</div>
+								<div class="remove">
+										<button class="btn btn-danger" type="button">REMOVE</button>
+								</div>
+						</div>`
+		cartRow.innerHTML = cartRowContents
+		cartImgBar.append(cartRow)
+		cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+		cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
 }
 
 function updateCartTotal()
 {
-    var cartItemContainer = document.getElementsByClassName('cart-img-bar')[0]
-    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
-    var total = 0
-    for(var i=0; i < cartRows.length; i++)
-    {
-        var cartRow = cartRows[i]
-        var priceElement = cartRow.getElementsByClassName('price')[0]
-        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-        var price = parseFloat(priceElement.innerText.replace('Price: Rs. ',''))
-        var quantity = quantityElement.value
-        total = total + (price * quantity)
-    }
-    total = Math.round(total*100) / 100
-    document.getElementsByClassName('cart-total-price')[0].innerText = 'Rs. ' + total
+		var cartItemContainer = document.getElementsByClassName('cart-img-bar')[0]
+		var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+		var total = 0
+		for(var i=0; i < cartRows.length; i++)
+		{
+				var cartRow = cartRows[i]
+				var priceElement = cartRow.getElementsByClassName('price')[0]
+				var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+				var price = parseFloat(priceElement.innerText.replace('Price: Rs. ',''))
+				var quantity = quantityElement.value
+				total = total + (price * quantity)
+		}
+		total = Math.round(total*100) / 100
+		document.getElementsByClassName('cart-total-price')[0].innerText = 'Rs. ' + total
 }
 
 var stripeHandler = StripeCheckout.configure({
-    key: stripePublicKey,
-    locale: 'auto',
-    token: function(token) {
-        var items = []
-        var cartItemContainer = document.getElementsByClassName('cart-img-bar')[0]
-        var cartRows = cartItemContainer.getElementsByClassName('cart-row')
-        for (var i = 0; i < cartRows.length; i++)
-        {
-            var cartRow = cartRows[i]
-            var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-            var quantity = quantityElement.value
-            var id = parseInt(cartRow.dataset.itemId)
-            items.push({
-                id: id,
-                quantity: quantity
-            })
-        }
+		key: stripePublicKey,
+		locale: 'auto',
+		token: function(token) {
+				var items = []
+				var cartItemContainer = document.getElementsByClassName('cart-img-bar')[0]
+				var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+				for (var i = 0; i < cartRows.length; i++)
+				{
+						var cartRow = cartRows[i]
+						var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+						var quantity = quantityElement.value
+						var id = parseInt(cartRow.dataset.itemId)
+						items.push({
+								id: id,
+								quantity: quantity
+						})
+				}
 
-        fetch('/purchase', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify({
-                stripeTokenId: token.id,
-                items: items
-            })
-        }).then(function() {
-            alert('Successfully purchased items')
-            while (cartItemContainer.hasChildNodes())
-            {
-                cartItemContainer.removeChild(cartItemContainer.firstChild)
-            }
-            updateCartTotal()
-        }).catch(function(error) {
-            console.error(error)
-        })
-    }
+				fetch('/purchase', {
+						method: 'POST',
+						headers: {
+								'Content-Type': 'application/json',
+								'Accept': 'application/json'
+						},
+						body: JSON.stringify({
+								stripeTokenId: token.id,
+								uid: userid,
+								items: items
+						})
+				}).then(function() {
+						alert('Successfully purchased items')
+						while (cartItemContainer.hasChildNodes())
+						{
+								cartItemContainer.removeChild(cartItemContainer.firstChild)
+						}
+						updateCartTotal()
+				}).catch(function(error) {
+						console.error(error)
+				})
+		}
 })
+
+function validatePhoneNumber(input_str) {
+	var re = /^[6-9]\d{9}$/gi;
+	return re.test(input_str)
+}
+
+function validateEmail(input_str) {
+	var re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+	return re.test(input_str)
+}
+
+var userid
 
 function purchaseClicked()
 {
-    var priceElement = document.getElementsByClassName('cart-total-price')[0]
-    var price = parseFloat(priceElement.innerText.replace('Rs. ','')*100)
-    stripeHandler.open({
-        amount: price,
-        currency: 'inr'
-    })
+		var i = 1
+		while(i)
+		{
+				userid = prompt('Enter your email/phone number to place the order:')
+				if(validatePhoneNumber(userid) || validateEmail(userid))
+				{
+					i = 0
+				}
+				else
+				{
+						alert('Invalid email/phone number.')
+				}
+		}
+		var priceElement = document.getElementsByClassName('cart-total-price')[0]
+		var price = parseFloat(priceElement.innerText.replace('Rs. ','')*100)
+		stripeHandler.open({
+				amount: price,
+				currency: 'inr'
+		})
 }
--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 if(process.env.NODE_ENV !== 'production')
 {
-    require('dotenv').config()
+		require('dotenv').config()
 }
 
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY
 const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
 
+
 const express = require('express')
 const app = express()
 const fs = require('fs')
 const stripe = require('stripe')(stripeSecretKey)
+
+const mysql = require('mysql2')
+var con = mysql.createConnection({
+					host: process.env.host,
+					user: process.env.user,
+					password: process.env.password,
+					database: process.env.database
+				});
+
+con.connect(function(err) {
+	if (err) throw err;
+	console.log("Database connected!");
+});
 
 app.set('view engine', 'ejs')
 app.use(express.json())
@@ -19,61 +33,62 @@ module.exports = app
 
 app.get('/menu', function(req, res)
 {
-    fs.readFile('items.json', function(error, data)
-    {
-        if(error)
-        {
-            res.status(500).end()
-        }
-        else
-        {
-            res.render('menu.ejs',
-            {
-                stripePublicKey: stripePublicKey,
-                items: JSON.parse(data)
-            })
-        }
-    })
+		fs.readFile('items.json', function(error, data)
+		{
+				if(error)
+				{
+						res.status(500).end()
+				}
+				else
+				{
+						res.render('menu.ejs',
+						{
+								stripePublicKey: stripePublicKey,
+								items: JSON.parse(data)
+						})
+				}
+		})
 })
 
 app.post('/purchase', function(req, res)
 {
-    fs.readFile('items.json', function(error, data)
-    {
-        if(error)
-        {
-            res.status(500).end()
-        }
-        else
-        {
-            const itemsJson = JSON.parse(data)
-            const itemsArray = itemsJson.storeItems
-            let total = 0
-            req.body.items.forEach(function(item) {
-                const itemJson = itemsArray.filter(function(i) {
-                    return (i.id == item.id)
-                })
-                total = total + (itemJson[0].price * parseInt(item.quantity))
-            })
-            total = total * 100
-            const paymentIntent = stripe.paymentIntents.create({
-                amount: total,
-                currency: 'inr',
-                automatic_payment_methods: {
-                    enabled: true,
-                }
-            }).then(function() {
-                console.log('Charge successful')
-            }).catch(function(error) {
-                console.log(error)
-                console.log('Charge failed')
-                res.status(500).end()
-            })
-            res.send({
-                clientSecret: paymentIntent.client_secret,
-            });
-        }
-    })
+		fs.readFile('items.json', function(error, data)
+		{
+				if(error)
+				{
+						res.status(500).end()
+				}
+				else
+				{
+						const itemsJson = JSON.parse(data)
+						const itemsArray = itemsJson.storeItems
+						let total = 0
+						req.body.items.forEach(function(item) {
+								const itemJson = itemsArray.filter(function(i) {
+										return (i.id == item.id)
+								})
+								total = total + (itemJson[0].price * parseInt(item.quantity))
+								console.log(itemJson[0].name, item.quantity, req.body.uid)
+						})
+						total = total * 100
+						const paymentIntent = stripe.paymentIntents.create({
+								amount: total,
+								currency: 'inr',
+								automatic_payment_methods: {
+										enabled: true,
+								}
+						}).then(function() {
+								console.log('Charge successful')
+						}).catch(function(error) {
+								console.log(error)
+								console.log('Charge failed')
+								res.status(500).end()
+						})
+						res.send({
+								clientSecret: paymentIntent.client_secret,
+						});
+				}
+		})
 })
 
 const port = process.env.PORT || 3000;
